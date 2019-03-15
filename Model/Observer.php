0e54a75b52c821aefd0a704584f0f958d566494c
Rodrigo Ananias <r.ananias03@gmail.com>
2019-03-15 09:17:39 -0300

    #11247 - Padroniza modulo checkoutsubscriber usando phpcs

--- Model/Observer.php
+++ Model/Observer.php
@@ -1,16 +1,37 @@
 <?php
-class Cammino_Checkoutsubscriber_Model_Observer {
-    public function subscribe($observer) {
+/**
+ * Observer.php
+ *
+ * @category Cammino
+ * @package  Cammino_Checkoutsubscriber
+ * @author   Cammino Digital <suporte@cammino.com.br>
+ * @license  http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @link     https://github.com/cammino/magento-checkoutsubscriber
+ */
+
+class Cammino_Checkoutsubscriber_Model_Observer
+{
+    /**
+     * Function responsible for add customer as subscriber
+     * after finish order
+     *
+     * @param string $observer Magento default observer
+     *
+     * @return null
+     */
+    public function subscribe($observer)
+    {
         $quote = $observer->getEvent()->getQuote();
         $customer = $quote->getCustomer();
 
-        switch ($quote->getCheckoutMethod()){
-            case Mage_Sales_Model_Quote::CHECKOUT_METHOD_REGISTER:
-                $customer->setIsSubscribed(1);
-                break;
-            case Mage_Sales_Model_Quote::CHECKOUT_METHOD_LOGIN_IN:
-                $customer->setIsSubscribed(1);
-                break;
+        switch ($quote->getCheckoutMethod()) {
+        case Mage_Sales_Model_Quote::CHECKOUT_METHOD_REGISTER:
+            $customer->setIsSubscribed(1);
+            break;
+
+        case Mage_Sales_Model_Quote::CHECKOUT_METHOD_LOGIN_IN:
+            $customer->setIsSubscribed(1);
+            break;
         }
     }
 }
